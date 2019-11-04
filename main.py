9e449b4067b25473f71dbf53efab9476c1aacffc
citrica <inma.rueda28@gmail.com>
2019-11-04 20:12:16 +0100

    Mejoras

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import json
 import datetime
 
 
-def play_game(levelSelect):
+def play_game(file, levelSelect):
     secretNumber = random.randint(1, 30)
     attempts = 0
     wrong_guesses = []
-    score_list = get_score_list()
+    score_list = get_score_list(file)
     current_time = datetime.datetime.now().strftime('%Y-%m-%d')
 
     playerName = input("Enter your name, please: ")
@@ -39,37 +39,35 @@ def play_game(levelSelect):
         wrong_guesses.append(guess)
 
 
-def get_score_list():  # lista de resultados
-    with open("score_list.txt", "r") as score_file:
+def get_score_list(file):  # lista de resultados
+    with open(file, "r") as score_file:
         a = score_file.read()
         score_list = json.loads(a)
 
         return score_list
 
 
-def get_top_scores():  # lista de mejores resultados
-    score_list = get_score_list()
+def get_top_scores(file):  # lista de mejores resultados
+    score_list = get_score_list(file)
     top_score_list = sorted(score_list, key=lambda k: k['attempts'])[:3]
-    print("Top 3: ")
-
-    for score_dict in top_score_list:
-        print("Player name: " + str(score_dict.get("name"))
-              + " / Secret number: " + str(score_dict.get("number"))
-              + " / Attempts: " + str(score_dict.get("attempts"))
-              + " / Wrong guesses: " + str(score_dict.get("listWrongNumbers"))
-              + " / Date: " + str(score_dict.get("date")))
     return top_score_list
 
 
 while True:
     selection = input("Would you like to A) play a new game, B) see the best scores, or C) quit? ")
     selection = selection.upper()
-
+    file = "score_list.txt"
     if selection == "A":
         level = input("Select game level (easy/hard): ")
         level.lower()
-        play_game(level)
+        play_game(file, level)
     elif selection == "B":
-        get_top_scores()
+        print("Top 3: ")
+        for score_dict in get_top_scores(file):
+            print("Player name: " + str(score_dict.get("name"))
+                  + " / Secret number: " + str(score_dict.get("number"))
+                  + " / Attempts: " + str(score_dict.get("attempts"))
+                  + " / Wrong guesses: " + str(score_dict.get("listWrongNumbers"))
+                  + " / Date: " + str(score_dict.get("date")))
     else:
         break
